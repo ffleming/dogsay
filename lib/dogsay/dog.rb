8f248ade3f523eeacbaf04153760225d69f232e4
Forrest Fleming <ffleming@oversee.net>
2015-04-03 10:45:23 -0700

    Load dog as YAML

--- lib/dogsay/dog.rb
+++ lib/dogsay/dog.rb
@@ -4,11 +4,23 @@ module Dogsay
 end
 class Dogsay::Dog
   def initialize(dog=:sit)
-    filename = File.join( File.dirname(__FILE__), 'dogs', "#{dog}.dog")
-    dog_hash = YAML.load_file filename
-    abort dog_hash.to_s
-    line_arr = File.open(filename).read.split("\n")
-    max_length = line_arr.map(&:length).max
-    line_arr.map { |l| l.ljust(max_length) }.join("\n")
+    filename = File.join( File.dirname(__FILE__), 'dogs', "#{dog}.dog2")
+    hsh = YAML.load_file filename
+    @dog = dog_from(hsh)
+    @text_position = text_position_from(hsh)
+  end
+
+  private
+
+  def dog_from(hash)
+    dog_lines = hash[:dog].split("\n")
+    max_length = dog_lines.map(&:length).max
+    dog_lines.map { |l| l.ljust(max_length) }.join("\n")
+  end
+
+  def text_position_from(hash)
+    abort hash[:text_position].class.to_s
+    
+    hash[:text_position]
   end
 end
