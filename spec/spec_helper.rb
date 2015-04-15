4175083ab7b38b97df8734e769fb2f509b8e8d8f
Forrest Fleming <ffleming@gmail.com>
2015-04-14 19:52:20 -0700

    Simplecov
    Add simplecov to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,8 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'simplecov'
+SimpleCov.start
 require_relative '../lib/dogsay'
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
