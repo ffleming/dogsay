5a9ef6a57f8be57fc244d343b92b4eb6d1cc0d66
Forrest Fleming <ffleming@gmail.com>
2015-04-15 20:30:14 -0700

    I like default of center better

--- lib/dogsay/text_box.rb
+++ lib/dogsay/text_box.rb
@@ -4,7 +4,7 @@ class Dogsay::TextBox
   def initialize(text, opts={})
     @text_width = opts.fetch(:text_width, 40)
     @separator = opts.fetch(:strip, false) ? ' ' : / /
-    @justify = opts.fetch(:justify, :ljust)
+    @justify = opts.fetch(:justify, :center)
     max_width = text.split("\n").map(&:length).max
     @raw = text.boxed(max_width + 4, justify: :ljust)
     @ascii = text.space_at(text_width - 4, on: separator).wrap(text_width - 4).boxed(text_width, justify: justify)
